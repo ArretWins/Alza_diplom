d4aa8004aeec7fdaeb3d40d720e4fe05965ee553
Artem <prohorenkoartem111@gmail.com>
2024-08-15 16:47:13 +0200

    update API user

--- services/user_service.py
+++ services/user_service.py
@@ -30,6 +30,16 @@ class UserService(BaseService):
             url = f'{self.url}/login?username={username}&password={password}'
             return self.get_request(url=url)
 
+    def get_user_by_username(self, username):
+        with allure.step('Get user by id'):
+            url = f'{self.url}/{username}'
+            return self.get_request(url=url)
+
+    def delete_user(self, username):
+        with allure.step('Delete user'):
+            url = f'{self.url}/{username}'
+            return self.delete_request(url=url, code=200)
+
     def update_user(self, old_username, username, first_name, last_name, email, password, phone_number):
         with allure.step('Update user'):
             url = f'{self.url}/{old_username}'
--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -3,15 +3,23 @@ from services.user_service import UserService
 
 def test_create_with_array():
     users = UserService()
-    response = users.create_user_with_array('ioioioio', 'Artem', "Wins",
-                                            'aaa@aa.ru', '12313', '21313124')
+    response = users.create_user_with_array('ioioioio', 'Bill', "Wins",
+                                            'aaa@aa.com', '12313', '21313124')
     assert response['message'] == 'ok'
+
+
+def test_get_user_by_username():
+    users = UserService()
+    username = 'ioioioio'
+    first_name = 'Bill'
+    response = users.get_user_by_username(username)
+    assert response['username'] == username
+    assert response['firstName'] == first_name
 
 
 def test_login():
     users = UserService()
     response = users.login_user('ioioioio', '12313')
-
     assert response['code'] == 200
 
 
@@ -19,5 +27,13 @@ def test_update_user():
     users = UserService()
     response = users.update_user('ioioioio',
                                  'Wins', 'Wiins', "Wiiins",
-                                 'Wins@aa.ru', '12313', '21313124')
+                                 'Wins@aa.com', '12313', '21313124')
     assert response['code'] == 200
+
+
+def test_delete_user():
+    users = UserService()
+    response = users.delete_user('ioioioio')
+    assert response['code'] == 200, f"Expected code 200, but got {response['code']}"
+    assert response['type'] == 'unknown', f"Expected type 'unknown', but got {response['type']}"
+    assert response['message'] == 'ioioioio', f"Expected message '375', but got {response['message']}"
